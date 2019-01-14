0ac2aaeb40cb4e92cce8c3f377294eb8a264e5b6
dangdanding <dangdanding@163.com>
2019-01-14 22:58:33 +0800

    dnp3

--- client.py
+++ client.py
@@ -6,22 +6,37 @@ from binascii import hexlify, unhexlify
 
 DEBUG = 1
 
-ReadRequest = "056405c903000400bd71"
-LinkReset = ""
-
 dnp3_HealthCheck="\x05\x64\x05\xc0\x01\x00\x00\x04\xe9\x21" 
+dnp3_HealthCheck="056405c001000004e921"
 dnp3_WarmRestart="\x05\x64\x08\xc4\x01\x00\x02\x00\x39\x0d\xde\xce\x0e\x6c\xd1" 
+dnp3_WarmRestart="056408c401000200390ddece0e6cd1" 
 dnp3_ColdRestart="\x05\x64\x08\xc4\x01\x00\x02\x00\x39\x0d\xde\xce\x0d\x8E\x8B" 
+dnp3_ColdRestart="056408c401000200390ddece0d8E8B" 
 dnp3_Write="\x05\x64\x08\xc4\x01\x00\x02\x00\x39\x0d\xde\xce\x02\x9d\xf7" 
+dnp3_Write="056408c401000200390ddece029df7" 
 dnp3_InitData="\x05\x64\x08\xc4\x01\x00\x02\x00\x39\x0d\xde\xce\x0f\x32\xe7" 
+dnp3_InitData="056408c401000200390ddece0f32e7" 
 dnp3_AppTermination="\x05\x64\x08\xc4\x01\x00\x02\x00\x39\x0d\xde\xce\x12\xf6\x45" 
+dnp3_AppTermination="056408c401000200390ddece12f645" 
 dnp3_DeleteFile="\x05\x64\x08\xc4\x01\x00\x02\x00\x39\x0d\xde\xce\x1b\x21\x8c" 
+dnp3_DeleteFile="056408c401000200390ddece1b218c" 
 dnp3_ReadRequest="056405c903000400bd71"
+
+dnp3_list=[
+           "056405c001000004e921",  #dnp3_HealthCheck
+           "056408c401000200390ddece0e6cd1", #dnp3_WarmRestart
+           "056408c401000200390ddece0d8E8B", #dnp3_ColdRestart
+           "056408c401000200390ddece029df7", #dnp3_Write
+           "056408c401000200390ddece0f32e7", #dnp3_InitData
+           "056408c401000200390ddece12f645", #dnp3_AppTermination
+           "056408c401000200390ddece1b218c", #dnp3_DeleteFile
+           "056405c903000400bd71"            #dnp3_ReadRequest
+    ]
 
 HOST = "127.0.0.1"
 PORT = 20000
 BUFSIZ = 1024
-TYPE = dnp3_ReadRequest
+
 
 def usage():
     print(sys.argv[0])
@@ -44,13 +59,32 @@ Defined DNP3 packet for parmater -t / --type:
 
     """)
 
-def debug():
+def debug(msg):
     if DEBUG == 1:
-        print ("debug: goes here...")
+        print ("debug: %s" % msg)
+
+
+def send_dnp3_packet(socket, dnp3_type = 8):
+ 
+    dnp3_pdu = dnp3_list[int(dnp3_type) - 1]
+    print  ("Sending DNP3 packet to target %s: %s"%(HOST, dnp3_pdu))
+    socket.sendall(unhexlify(dnp3_pdu))
+
+    #get response
+    if (8 == int(dnp3_type)):
+        resp = socket.recv(BUFSIZ)
+        print("Received DNP3 Response from %s: %s" % (HOST, resp.encode('hex')))
+ 
+
+def debug(msg):
+    if DEBUG == 1:
+        print ("debug: %s" % msg)
 
 def main(argv):
+    DNP3_type = 8  #default to Request Link Status, if not specify DNP3 packet attack type [1..8]
+
     try:
-        opts,args = getopt.getopt(sys.argv[1:],"hp:i:t:",["help","ip=","port=","type"])
+        opts,args = getopt.getopt(sys.argv[1:],"hp:i:t:",["help","ip=","port=","type="])
 
     except getopt.GetoptError:
         usage()
@@ -67,12 +101,18 @@ def main(argv):
              PORT= arg
              print  ("Destination PORT: %s"% PORT)
           elif opt in ("-t", "--type"):
-             TYPE= arg
-             print  ("DNP3 type: %s"% TYPE)
+             try:
+                 DNP3_type= int(arg)
+             except: 
+                 print ("DNP3 type %s is NOT recogizable!" % arg)
+                 sys.exit(1)
+             if (DNP3_type > len(dnp3_list)):
+                     print ("DNP3 type %s is out of range [1-%s]!" % (arg, len(dnp3_list)))
+                     sys.exit(1)
+             print  ("DNP3 injected type: %s"% DNP3_type)
+           
+                
 
-
-    #debug()
- 
     try:
         tcpCliSock = socket(AF_INET, SOCK_STREAM)
     except error, e:
@@ -86,14 +126,8 @@ def main(argv):
     except error, e:
         print  ("Connect to target %s: %s error: %s" % (HOST, PORT, e))
  
-    #sending request
-    print  ("Sending Link Status Request to Source %s: %s"%(HOST, ReadRequest))
-    tcpCliSock.sendall(unhexlify(ReadRequest))
+    send_dnp3_packet(tcpCliSock, DNP3_type)
 
-    #get response
-    resp = tcpCliSock.recv(BUFSIZ)
-    print("Received Link Status Response from %s: %s" % (HOST, resp.encode('hex')))
- 
     str1 = raw_input('> ')
     tcpCliSock.close()
 
--- svr.py
+++ svr.py
@@ -9,8 +9,18 @@ PORT = 20000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
  
-ReadRequest="056405c903000400bd71"
 ResponseLinkStatus="0564000b040003000000"
+dnp3_list=[
+           "056405c001000004e921",  #dnp3_HealthCheck
+           "056408c401000200390ddece0e6cd1", #dnp3_WarmRestart
+           "056408c401000200390ddece0d8E8B", #dnp3_ColdRestart
+           "056408c401000200390ddece029df7", #dnp3_Write
+           "056408c401000200390ddece0f32e7", #dnp3_InitData
+           "056408c401000200390ddece12f645", #dnp3_AppTermination
+           "056408c401000200390ddece1b218c", #dnp3_DeleteFile
+           "056405c903000400bd71"            #dnp3_ReadRequest
+    ]
+
 
 def debug():
     if DEBUG == 1:
@@ -34,8 +44,10 @@ def main():
             rcv_msg = tcpCliSock.recv(BUFSIZ)
             if not rcv_msg:
                 break
-            print("Received Link Status Request from %s: %s" % (addr[0], hexlify(rcv_msg)))
-            if hexlify(rcv_msg) == ReadRequest:
+
+            print("Received DNP3 packet from %s: %s" % (addr[0], hexlify(rcv_msg)))
+
+            if hexlify(rcv_msg) == dnp3_list[7]:
                 print ("Sending Link Status Response to %s: %s" %(addr[0],ResponseLinkStatus))
                 tcpCliSock.sendall(unhexlify(ResponseLinkStatus))
  
